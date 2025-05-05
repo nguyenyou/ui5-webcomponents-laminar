4c97f5ff671497108053fc3b0d636a140cf0d98f
nguyenyou <tunt081295@gmail.com>
2025-05-06 03:13:27 +0700

    🚢 ship it

--- website/src/website/components/Sidebar.scala
+++ website/src/website/components/Sidebar.scala
@@ -9,26 +9,11 @@ import website.facades.scrollIntoViewIfNeeded
 import org.scalajs.dom
 import website.facades.ScrollOptions
 import io.github.nguyenyou.scalawind.*
-import website.extensions.scalawind.border_grid
-import website.extensions.scalawind.no_scrollbar
-import website.extensions.scalawind.text_sap_text
+import website.extensions.scalawind.*
 case class Sidebar() {
   private case class IndicatorStyles(height: Int, width: Int, top: Int)
 
   private val activeLinkRefVar = Var[Option[dom.Element]](None)
-
-  private val hoverLinkVar    = Var[Option[Page]](None)
-  private val hoverLinkSignal = hoverLinkVar.signal.distinct
-
-  private val hoverStylesVar = Var[IndicatorStyles](
-    IndicatorStyles(
-      0,
-      0,
-      0
-    )
-  )
-
-  private val hoverStylesSignal = hoverStylesVar.signal
 
   val scrollContainer = div(tw.relative.h_full.overflow_y_auto)
 
@@ -40,43 +25,17 @@ case class Sidebar() {
     val isActiveSignal = currentPageSignal.map(_ == page).distinct
     tag
       .amend(
-        tw.group.relative.flex.h_8.w_full.items_center.rounded_lg.px_2.font_normal.text_sap_text,
-        // tw.font_medium <-- isActiveSignal,
+        tw.group.relative.flex.h_8.w_full.items_center.rounded_lg.px_3.font_normal.text_sap_text,
+        tw.font_medium.sidebar_item_hover.text_sap_brand <-- isActiveSignal,
+        tw.hover(tw.sidebar_item_hover).transition_all.ease_linear,
         navigateTo(page),
         title.getOrElse(page.title),
         isActiveSignal --> Observer[Boolean] { isActive =>
           if (isActive) {
             activeLinkRefVar.set(Some(tag.ref))
           }
-        },
-        onMouseLeave.mapTo(None) --> hoverLinkVar
-      )
-      .amendThis { thisNode =>
-        onMouseEnter --> Observer { _ =>
-          Var.set(
-            hoverLinkVar -> Some(page),
-            hoverStylesVar ->
-              IndicatorStyles(
-                thisNode.ref.offsetHeight.toInt,
-                thisNode.ref.offsetWidth.toInt,
-                thisNode.ref.offsetTop.toInt - scrollContainer.ref.scrollTop.toInt
-              )
-          )
         }
-      }
-  }
-
-  private val hoverIndicator: HtmlElement = {
-    div(
-      tw.absolute.mx_2.rounded.transition_all.duration_300.ease_out,
-      cls <-- hoverLinkSignal.map {
-        case Some(_) => tw.opacity_100.css
-        case None    => tw.opacity_0.css
-      },
-      top.px <-- hoverStylesSignal.map(_.top),
-      height.px <-- hoverStylesSignal.map(_.height),
-      width.px <-- hoverStylesSignal.map(_.width)
-    )
+      )
   }
 
   private val scrollToViewSubscription =
@@ -99,8 +58,9 @@ case class Sidebar() {
 
   def apply(): HtmlElement = {
     asideTag(
-      tw.border_grid.fixed.top_14.z_30.hidden.raw("h-[calc(100vh-3.5rem)]"),
+      tw.relative.border_grid.fixed.top_14.z_30.hidden,
       tw.w_full.shrink_0.border_r.md(tw.sticky.block),
+      cls("h-[calc(100vh-3.5rem)]"),
       scrollContainer.amend(
         tw.no_scrollbar.h_full.overflow_auto.py_6.pr_4.lg(tw.py_8),
         div(
--- website/src/website/extensions/scalawind/scalawind.scala
+++ website/src/website/extensions/scalawind/scalawind.scala
@@ -7,9 +7,11 @@ extension (tw: Tailwind) {
   def bg_sap_background: Tailwind  = tw
   def bg_sap_neutral: Tailwind     = tw
   def text_sap_text: Tailwind      = tw
+  def text_sap_brand: Tailwind     = tw
   def border_sap_neutral: Tailwind = tw
   def border_grid: Tailwind        = tw
   def border_border: Tailwind      = tw
   def container_wrapper: Tailwind  = tw
   def no_scrollbar: Tailwind       = tw
+  def sidebar_item_hover: Tailwind = tw
 }
