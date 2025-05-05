da38dbea81c951d208fba6605d18618757162f20
nguyenyou <tunt081295@gmail.com>
2025-05-06 03:16:57 +0700

    🚢 ship it

--- website/src/website/views/docs/BreadcrumbsView.scala
+++ website/src/website/views/docs/BreadcrumbsView.scala
@@ -26,6 +26,130 @@ object BreadcrumbsView extends ExampleView("Breadcrumbs") {
             )("Current page")
           )
         }
+      ),
+      Demo(
+        title = "Overflow",
+        content = Source.annotate {
+          Breadcrumbs()(
+            BreadcrumbsItem(
+              _.href   := "https://www.sap.com",
+              _.target := "_blank"
+            )("Root Page"),
+            BreadcrumbsItem(
+              _.href   := "https://www.sap.com",
+              _.target := "_blank"
+            )("Parent Page1"),
+            BreadcrumbsItem(
+              _.href   := "https://www.sap.com",
+              _.target := "_blank"
+            )("Parent Page2"),
+            BreadcrumbsItem(
+              _.href   := "https://www.sap.com",
+              _.target := "_blank"
+            )("Parent Page3"),
+            BreadcrumbsItem(
+              _.href   := "https://www.sap.com",
+              _.target := "_blank"
+            )("Parent Page4"),
+            BreadcrumbsItem(
+              _.href   := "https://www.sap.com",
+              _.target := "_blank"
+            )("Parent Page5"),
+            BreadcrumbsItem(
+              _.href   := "https://www.sap.com",
+              _.target := "_blank"
+            )("Parent Page6"),
+            BreadcrumbsItem(
+              _.href   := "https://www.sap.com",
+              _.target := "_blank"
+            )("Parent Page7"),
+            BreadcrumbsItem(
+              _.href   := "https://www.sap.com",
+              _.target := "_blank"
+            )("Parent Page8"),
+            BreadcrumbsItem(
+              _.href   := "https://www.sap.com",
+              _.target := "_blank"
+            )("Parent Page9"),
+            BreadcrumbsItem(
+              _.href   := "https://www.sap.com",
+              _.target := "_blank"
+            )("Parent Page10"),
+            BreadcrumbsItem()("Current Page")
+          )
+        }
+      ),
+      Demo(
+        title = "Separator Styles",
+        content = Source.annotate {
+          div(
+            div(
+              Breadcrumbs(
+                _.separators := "BackSlash"
+              )(
+                BreadcrumbsItem(
+                  _.href := "https://www.sap.com"
+                )("Root Page"),
+                BreadcrumbsItem(
+                  _.href := "https://www.sap.com"
+                )("Parent Page"),
+                BreadcrumbsItem()("Current Page")
+              )
+            ),
+            div(
+              Breadcrumbs(
+                _.separators := "DoubleBackSlash"
+              )(
+                BreadcrumbsItem(
+                  _.href := "https://www.sap.com"
+                )("Root Page"),
+                BreadcrumbsItem(
+                  _.href := "https://www.sap.com"
+                )("Parent Page"),
+                BreadcrumbsItem()("Current Page")
+              )
+            ),
+            div(
+              Breadcrumbs(
+                _.separators := "DoubleGreaterThan"
+              )(
+                BreadcrumbsItem(
+                  _.href := "https://www.sap.com"
+                )("Root Page"),
+                BreadcrumbsItem(
+                  _.href := "https://www.sap.com"
+                )("Parent Page"),
+                BreadcrumbsItem()("Current Page")
+              )
+            ),
+            div(
+              Breadcrumbs(
+                _.separators := "DoubleSlash"
+              )(
+                BreadcrumbsItem(
+                  _.href := "https://www.sap.com"
+                )("Root Page"),
+                BreadcrumbsItem(
+                  _.href := "https://www.sap.com"
+                )("Parent Page"),
+                BreadcrumbsItem()("Current Page")
+              )
+            ),
+            div(
+              Breadcrumbs(
+                _.separators := "GreaterThan"
+              )(
+                BreadcrumbsItem(
+                  _.href := "https://www.sap.com"
+                )("Root Page"),
+                BreadcrumbsItem(
+                  _.href := "https://www.sap.com"
+                )("Parent Page"),
+                BreadcrumbsItem()("Current Page")
+              )
+            )
+          )
+        }
       )
     )
   }
