32982d5ede1fb199d9fc1e8433dcb424bf12f6f1
nguyenyou <tunt081295@gmail.com>
2025-05-08 14:16:33 +0700

    🚢 ship it

--- website/src/website/components/Preview.scala
+++ website/src/website/components/Preview.scala
@@ -59,7 +59,7 @@ object Preview {
       case "Code"    => codeNode
     }
 
-    val PreviewTabWidth = 99
+    val PreviewTabWidth = 107
 
     val initialIndicatorStyles =
       IndicatorStyles(PreviewTabWidth, initialNode.ref.offsetLeft.toInt)
