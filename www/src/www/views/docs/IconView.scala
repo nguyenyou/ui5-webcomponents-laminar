95af4355f221733fdb33af1db829a86c76f586b9
nguyenyou <tunt081295@gmail.com>
2025-05-11 13:34:08 +0700

    🚢 ship it

--- www/src/www/views/docs/IconView.scala
+++ www/src/www/views/docs/IconView.scala
@@ -30,6 +30,87 @@ object IconView extends ExampleView("Icon") {
             )()
           )
         }
+      ),
+      Demo(
+        title = "Design",
+        content = Source.annotate {
+          div(
+            Icon(
+              _.name   := IconName.da2,
+              _.design := "Default"
+            )(),
+            Icon(
+              _.name   := IconName.da2,
+              _.design := "Contrast"
+            )(),
+            Icon(
+              _.name   := IconName.da2,
+              _.design := "Critical"
+            )(),
+            Icon(
+              _.name   := IconName.da2,
+              _.design := "Negative"
+            )(),
+            Icon(
+              _.name   := IconName.da2,
+              _.design := "Positive"
+            )(),
+            Icon(
+              _.name   := IconName.da2,
+              _.design := "Information"
+            )(),
+            Icon(
+              _.name   := IconName.da2,
+              _.design := "Neutral"
+            )(),
+            Icon(
+              _.name   := IconName.da2,
+              _.design := "NonInteractive"
+            )()
+          )
+        }
+      ),
+      Demo(
+        title = "Custom Styling",
+        content = Source.annotate {
+          div(
+            Icon(
+              _.name := IconName.home
+            )(
+              width.rem(2),
+              height.rem(2),
+              color("blueviolet")
+            ),
+            Icon(
+              _.name := IconName.ai
+            )(
+              width.rem(2),
+              height.rem(2),
+              color("orangered")
+            ),
+            Icon(
+              _.name := IconName.da2
+            )(
+              width.rem(2),
+              height.rem(2),
+              color("orange")
+            ),
+            Icon(
+              _.name := IconName.heart
+            )(
+              width.rem(2),
+              height.rem(2),
+              color("red")
+            ),
+            Icon(
+              _.name := IconName.activities
+            )(
+              width.rem(2),
+              height.rem(2),
+              color("olivedrab")
+            )
+          )
+        }
       )
     )
   }
