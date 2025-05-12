ef789f69e3abaa488e091a0345c5bd2bc8e03949
nguyenyou <tunt081295@gmail.com>
2025-05-12 11:52:05 +0700

    🚢 ship it

--- www/src/www/views/docs/InputView.scala
+++ www/src/www/views/docs/InputView.scala
@@ -16,6 +16,77 @@ object InputView extends ExampleView("Input") {
             _.value := "Input"
           )()
         }
+      ),
+      Demo(
+        title = "Clear Icon",
+        content = Source.annotate {
+          Input(
+            _.value         := "Input",
+            _.showClearIcon := true
+          )()
+        }
+      ),
+      Demo(
+        title = "Input with Suggestions",
+        content = Source.annotate {
+          val valueVar    = Var("")
+          val valueSignal = valueVar.signal.distinct
+          val databaseEntries = Seq(
+            "Argentina",
+            "Albania",
+            "Algeria",
+            "Angola",
+            "Austria",
+            "Australia",
+            "Bulgaria",
+            "Canada",
+            "Columbia",
+            "Croatia",
+            "Denmark",
+            "England",
+            "Finland",
+            "France",
+            "Germany",
+            "Hungary",
+            "Ireland",
+            "Italy",
+            "Kuwait",
+            "Luxembourg",
+            "Mexico",
+            "Morocco",
+            "Norway",
+            "Paraguay",
+            "Philippines",
+            "Portugal",
+            "Spain",
+            "Sweden",
+            "Sri Lanka",
+            "Senegal",
+            "Thailand",
+            "The United Kingdom of Great Britain and Northern Ireland",
+            "USA"
+          )
+          val suggestionsSignal = valueSignal.map { value =>
+            if (value.nonEmpty) {
+              databaseEntries.filter(entry => entry.toLowerCase.contains(value.toLowerCase))
+            } else {
+              Seq.empty[String]
+            }
+          }
+
+          Input(
+            _.placeholder     := "Type 'a'",
+            _.showSuggestions := true,
+            _.value <-- valueVar.signal.distinct,
+            _.onInput.map(_.target.value) --> valueVar.writer
+          )(
+            children <-- suggestionsSignal.map {
+              _.map { suggestion =>
+                SuggestionItem(_.text := suggestion)()
+              }
+            }
+          )
+        }
       )
     )
   }
